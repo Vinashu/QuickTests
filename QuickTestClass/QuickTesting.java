de40676e2c159c0b48cab092add6b2cd448849ce
Rogério de Leon Pereira <vinashu@gmail.com>
2022-03-10 11:52:27 -0600

    Update QuickTesting.java
    totalTests, totalPass and totalFail are static variables, so we need to reset them in the begin() method.
    
    I added some code in the testingComplete() and testPrintOut() methods do add some colours. I know it works on MacOS, probably will work in any unix like systems (need to test), but I am not sure if it will work on Windows.

--- QuickTestClass/QuickTesting.java
+++ QuickTestClass/QuickTesting.java
@@ -5,6 +5,9 @@ public class QuickTesting {
 	private static boolean detailed = false;
 	
 	public static void begin() {
+		totalTests = 0;
+		totalPass = 0;
+		totalFail = 0;			
 		System.out.println("Simple Testing Begins...");
 	}
 	
@@ -36,7 +39,11 @@ public class QuickTesting {
 	
 	private static void testPrintOut(String message, boolean result) {
 		System.out.println("Test Performed: " + message + "\n" +
-		"Passed(?): " + result + "\n");
+		// Use like this if on Windows		   
+		// "Passed(?): " + result + "\n");
+				   
+		// Use like this if o Mac or unix like systems
+		"Passed(?): " + (result ? (char)27 + "[32m" : (char)27 + "[31m") + result + "\n" + (char)27 + "[39m");				   
 	}
 	
 	public static void end() {
@@ -45,8 +52,17 @@ public class QuickTesting {
 	}
 	
 	private static void testingComplete() {
-		System.out.println("Total tests: " + totalTests +
-		"\nTotal Passed: " + totalPass +
-		"\nTotal Failed: " + totalFail);
+		// Use like this if on Windows
+		// System.out.println("Total tests: " + totalTests);
+		// System.out.println("Total Passed: " + totalPass);
+		// System.out.println("Total Failed: " + totalFail);
+		
+		//Use like this if on Mac or unix like systems
+		System.out.println((char)27 + "[36m" + "----------------------");
+		System.out.println((char)27 + "[33m" + "Total tests: " + totalTests);
+		System.out.println((char)27 + "[32m" + "Total Passed: " + totalPass);
+		System.out.println((char)27 + "[31m" + "Total Failed: " + totalFail);
+		System.out.print((char)27 + "[36m" + "----------------------");
+		System.out.println((char)27 + "[39m");
 	}
 }
